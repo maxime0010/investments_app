94256d50800a72e6070d1f8c5a1a661c8dea3ad1
maxime0010 <maxime0010@gmail.com>
2024-08-25 14:52:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('BREVO_EMAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('BREVO_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'hello@goodlife.money')
 
 mail = Mail(app)
 
