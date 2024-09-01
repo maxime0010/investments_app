05c988f484ff528c4612860e19566ca335511756
maxime0010 <maxime0010@gmail.com>
2024-09-01 18:48:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import mysql.connector
 import os
 import stripe
 import logging
+import requests
 from datetime import datetime
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
