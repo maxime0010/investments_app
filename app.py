36a6ca51f52982ac037ed58d277ca099dd975b23
maxime0010 <maxime0010@gmail.com>
2024-10-20 17:39:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ configuration.api_key['api-key'] = os.getenv('BREVO_API_KEY')
 
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp-relay.sendinblue.com'
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('BREVO_EMAIL')
