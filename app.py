bc2d5fdfbf7d032745494686c3748dab06b37e7b
maxime0010 <maxime0010@gmail.com>
2024-10-20 17:10:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('BREVO_EMAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('BREVO_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'maxime@maximegfeller.me')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'hello@maximegfeller.me')
 
 mail = Mail(app)
 
